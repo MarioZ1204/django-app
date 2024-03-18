7e63d898e4c623ab51f6493a20a365571800250e
MarioZ1204 <80787742+MarioZ1204@users.noreply.github.com>
2024-03-18 11:36:24 -0500

    New commit

--- app/academics/models.py
+++ app/academics/models.py
@@ -1,21 +1,65 @@
 import datetime
 from django.db import models
 
-# Create your models here.
-#usamos models.Model para usar los timpos de campos ejemplos variables
+
 class User(models.Model):
     email = models.EmailField(null = True, blank = True) 
     password = models.CharField(null = True, blank = True)
     status = models.BooleanField(default= True)
-    created_at = models.DateTimeField(default = datetime.datetime.now()) # blank = true que acaepte valores null
+    created_at = models.DateTimeField(default = datetime.datetime.now()) 
     updated_at = models.DateTimeField(default = datetime.datetime.now())
     deleted_at = models.DateTimeField(null = True, blank = True)
 
 class Person (models.Model):
     firstname = models.CharField(max_length = 20) 
     lastname = models.CharField(max_length = 20)
-    age = models.IntegerField()
-    ident_number = models.CharField(max_length = 12, blank = True) # blank = true que acaepte valores null
+    age = models.IntegerField(null = True)
+    ident_exp_city = models.IntegerField(null = True)
+    address = models.CharField(max_length = 150, blank = True)
+    mobile = models.CharField(max_length = 50, blank = True)
+    ident_number = models.CharField(max_length = 12, blank = True) 
+    id_user = models.IntegerField(null = True)
+    created_at = models.DateTimeField(default = datetime.datetime.now())
+    updated_at = models.DateTimeField(default = datetime.datetime.now())
+    deleted_at = models.DateTimeField(null = True, blank = True)
+    
+class Students(models.Model):
+    code = models.CharField(max_length = 20)
+    status = models.BooleanField(null = True, blank = True, default = True)
+    created_at = models.DateTimeField(default = datetime.datetime.now())
+    updated_at = models.DateTimeField(default = datetime.datetime.now())
+    deleted_at = models.DateTimeField(null = True, blank = True)
+
+class identification_types(models.Model):
+    name = models.CharField(max_length = 50)
+    abrev = models.CharField(max_length = 10)
+    descrip =  models.CharField(max_length = 100)
+    created_at = models.DateTimeField(default = datetime.datetime.now())
+    updated_at = models.DateTimeField(default = datetime.datetime.now())
+    deleted_at = models.DateTimeField(null = True, blank = True)
+
+class city(models.Model):
+    name = models.CharField(max_length = 100)
+    abrev = models.CharField(max_length = 10)  
+    descrip = models.CharField(max_length = 10)
+    id_dept = models.IntegerField(null = True) 
+    created_at = models.DateTimeField(default = datetime.datetime.now())
+    updated_at = models.DateTimeField(default = datetime.datetime.now())
+    deleted_at = models.DateTimeField(null = True, blank = True)
+
+class departments(models.Model):
+    name = models.CharField(max_length = 100)
+    abrev = models.CharField(max_length = 10)  
+    descrip = models.CharField(max_length = 10)
+    id_country = models.IntegerField(null = True) 
+    created_at = models.DateTimeField(default = datetime.datetime.now())
+    updated_at = models.DateTimeField(default = datetime.datetime.now())
+    deleted_at = models.DateTimeField(null = True, blank = True)
+
+class countries(models.Model):
+    name = models.CharField(max_length = 100)
+    abrev = models.CharField(max_length = 10)  
+    descrip = models.CharField(max_length = 10)
     created_at = models.DateTimeField(default = datetime.datetime.now())
     updated_at = models.DateTimeField(default = datetime.datetime.now())
     deleted_at = models.DateTimeField(null = True, blank = True)
--- app/app_web/settings.py
+++ app/app_web/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -75,17 +77,17 @@ WSGI_APPLICATION = 'app_web.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+
+# Database
 DATABASES = {
-    'default': {
-        #'ENGINE': 'django.db.backends.sqlite3',
-        #'NAME': BASE_DIR / 'db.sqlite3',
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'app',
-        'USER': 'postgres',
-        'PASSWORD': 'unicesmag',
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
+ 'default': {
+ 'ENGINE': 'django.db.backends.postgresql',
+ 'NAME': config('DB_NAME'),
+ 'USER': config('DB_USER'),
+ 'PASSWORD': config('DB_PASSWORD'),
+ 'HOST': config('DB_HOST'),
+ 'PORT': config('DB_PORT'),
+ }
 }
 
 
