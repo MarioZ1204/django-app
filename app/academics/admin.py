e56032c2c1561589be97bb70fa5a7b5c5adf98db
MarioZ1204 <mariozamb1204@gmail.com>
2024-03-19 12:06:06 -0500

    New commit

--- app/academics/admin.py
+++ app/academics/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+from.models import User, Person, Students, identification_types, city, departments, countries
 # Register your models here.
+
+admin.site.register(User)
+admin.site.register(Person)
+admin.site.register(Students)
+admin.site.register(identification_types)
+admin.site.register(city)
+admin.site.register(departments)
+admin.site.register(countries)
